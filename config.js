e833ebeb311db7bdd0bd8b0cb96a06f6a47561dc
SIMVCorporation <55985699+SIMVCorporation@users.noreply.github.com>
2019-09-30 10:29:58 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+
+host = 'mqtt.dioty.co';	// Nosso servidor Broker
+port = 1883; // Porta
+topic = '/simvcorporation@gmail.com/teste/mqtt';		// topic to subscribe to
+useTLS = false;
+username = simvcorporation@gmail.com;
+password = dad74363;
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
