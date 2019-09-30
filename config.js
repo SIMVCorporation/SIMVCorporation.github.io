2ccdcee6ba6e33496646f90bc7f3f511491c20eb
SIMVCorporation <55985699+SIMVCorporation@users.noreply.github.com>
2019-09-30 10:52:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ password = 'dad74363';
 //    defaults to "/mqtt"
 //    may include query and fragment
 //
-path = "";
-path = "";
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
 
 cleansession = true;
