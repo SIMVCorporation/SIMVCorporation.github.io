b256dbc6faced32d87e6df19fd8be4ea2bcd88ec
SIMVCorporation <55985699+SIMVCorporation@users.noreply.github.com>
2019-09-30 10:40:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-
-host = 'mqtt.dioty.co';	// Nosso servidor Broker
-port = 1883; // Porta
-topic = '/simvcorporation@gmail.com/teste/mqtt';		// topic to subscribe to
+// host = 'mqtt.dioty.co';	// hostname or IP address
+host = 'mqtt.dioty.co';	// hostname or IP address
+// host = 'mqtt.dioty.co';	// hostname or IP address
+port = 1883;
+topic = '/simvcorporation@gmail.com/testes/mqtt';		// topic to subscribe to
 useTLS = false;
 username = simvcorporation@gmail.com;
 password = dad74363;
+// username = "simvcorporation@gmail.com";
+// password = "dad74363";
 
 // path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
 //    defaults to "/mqtt"
