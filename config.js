8149d2a072faa0ad589ef3f7a6915c6eab89442d
SIMV <simvcorporation@gmail.com>
2019-10-19 00:35:39 -0300

    2019.10.19 - 00:35

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
                     var mqtt = require('mqtt');
 
 					
-                    var client = mqtt.connect("mqtts://postman.cloudmqtt.com:30281", {
+                    var client = mqtt.connect("mqtts://postman.cloudmqtt.com:10281", {
                     username: 'fdbljoqc',
                     password: 'nVpPA7ekAFeW'
                     });
