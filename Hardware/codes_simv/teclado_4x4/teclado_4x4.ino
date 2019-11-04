9d567ae5a848056f4c9514ecf9c62348de44ec82
Simv_re <simvcorporation@gmail.com>
2019-11-04 10:38:21 -0300

    Teclado 4x4

--- Hardware/codes_simv/teclado_4x4/teclado_4x4.ino
+++ Hardware/codes_simv/teclado_4x4/teclado_4x4.ino
@@ -1,80 +1,40 @@
-//https://www.filipeflop.com/blog/teclado-matricial-4x4-arduino/
-//Programa : Teste teclado matricial 4x4
-//Autor : FILIPEFLOP
+  
+#include <Keypad.h> //INCLUSÃO DE BIBLIOTECA
  
-void setup()
-{
-  //Pinos ligados aos pinos 1, 2, 3 e 4 do teclado - Linhas
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-   
-  //Pinos ligados aos pinos 5, 6, 7 e 8 do teclado - Colunas
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-   
-  Serial.begin(9600);
-  Serial.println("Aguardando acionamento das teclas...");
-  Serial.println();
+const byte qtdLinhas = 4; //QUANTIDADE DE LINHAS DO TECLADO
+const byte qtdColunas = 4; //QUANTIDADE DE COLUNAS DO TECLADO
+ 
+//CONSTRUÇÃO DA MATRIZ DE CARACTERES
+char matriz_teclas[qtdLinhas][qtdColunas] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+ 
+ 
+byte PinosqtdLinhas[qtdLinhas] = {3, 4, 5, 6}; //PINOS UTILIZADOS PELAS LINHAS
+byte PinosqtdColunas[qtdColunas] = {8, 9, 10,11}; //PINOS UTILIZADOS PELAS COLUNAS
+  
+//INICIALIZAÇÃO DO TECLADO
+Keypad meuteclado = Keypad( makeKeymap(matriz_teclas), PinosqtdLinhas, PinosqtdColunas, qtdLinhas, qtdColunas);
+ 
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600); //INICIALIZA A SERIAL
+  Serial.println("Aperte uma tecla..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+  Serial.println(); //QUEBRA UMA LINHA NO MONITOR SERIAL
 }
- 
-void loop()
-{
-    for (int ti = 3; ti<7; ti++)
-    {
-    //Alterna o estado dos pinos das linhas
-    digitalWrite(3, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(ti, HIGH);
-    //Verifica se alguma tecla da coluna 1 foi pressionada
-    if (digitalRead(8) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 1);
-      while(digitalRead(8) == HIGH){}
-    }
- 
-    //Verifica se alguma tecla da coluna 2 foi pressionada    
-    if (digitalRead(9) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 2);
-      while(digitalRead(9) == HIGH){};
-    }
-     
-    //Verifica se alguma tecla da coluna 3 foi pressionada
-    if (digitalRead(10) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 3);
-      while(digitalRead(10) == HIGH){}
-    }
-     
-    //Verifica se alguma tecla da coluna 4 foi pressionada
-    if (digitalRead(11) == HIGH)
-    {
-      imprime_linha_coluna(ti-2, 4);
-      while(digitalRead(11) == HIGH){} 
-    }
-   }
-   delay(10);
-}
- 
-void imprime_linha_coluna(int x, int y)
-{
-       Serial.print("Linha : ");
-       Serial.print(x);
-       Serial.print(" x Coluna : ");
-       Serial.print(y);
-       delay(10);
-       Serial.println();
-}
-
-void LigaMotor(){
-  digitalWrite(porta_rele1, LOW);
-  while(venda.confirmada){
-    
+  
+void loop(){
+  char tecla_pressionada = meuteclado.getKey(); //VERIFICA SE ALGUMA DAS TECLAS FOI PRESSIONADA
+  if (tecla_pressionada){ //SE ALGUMA TECLA FOR PRESSIONADA, FAZ
+    Serial.print("Tecla pressionada : "); //IMPRIME O TEXTO NO MONITOR SERIAL
+    Serial.println(tecla_pressionada); //IMPRIME NO MONITOR SERIAL A TECLA PRESSIONADA
+  }
+  if (tecla_pressionada == '1'){
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   }
 }
