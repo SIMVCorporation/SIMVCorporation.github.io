1136708b9bc032c479a02f3cd3b4dc3f1abe2364
SIMV <simvcorporation@gmail.com>
2019-11-20 16:15:01 -0300

    Formatação

--- config.js
+++ config.js
@@ -6,30 +6,29 @@ var client = mqtt.connect("mqtts://postman.cloudmqtt.com:30281", {
 });
 
 client.on('connect', function() {
-	client.subscribe('SIMV/relatorio', function() {
-		client.on('message', function(topic, message, packet) {
-		console.log("Received '" + message + "' on '" + topic + "'");
-			var result = message.includes('Temperatura');
-			var umidade = message.includes('Umidade');
-			console.log(result);
-			console.log(umidade);
-				if(result == true){
-					var span = document.querySelector('span')
-					span.innerHTML = message
-				} else if(umidade == true) 	{
-					var span = document.querySelector('span2')
-					span.innerHTML = message
-				} else {
-					var span = document.querySelector('span3')
-					span.innerHTML = message	
-				}							
+	var result = message.includes('Temperatura');
+	var umidade = message.includes('Umidade');
+		client.subscribe('SIMV/relatorio', function() {
+			client.on('message', function(topic, message, packet) {
+				console.log("Received '" + message + "' on '" + topic + "'");
+				console.log(result);
+				console.log(umidade);
+					if(result == true){
+						var span = document.querySelector('span2')
+						span.innerHTML = message
+					} else if(umidade == true) 	{
+						var span = document.querySelector('span3')
+						span.innerHTML = message
+					} else {
+						var span = document.querySelector('span4')
+						span.innerHTML = message	
+					}							
+			});
 		});
-	});
-	
 	client.publish('SIMV/status', 'Site ONLINE', function() {
 		console.log("Message posted...");	
 		var img = document.getElementById("SiteStatus");
-		img.src = "/Media.Style/SiteOn.png";
+		img.src = "/images/SiteOn.png";
 	});
 });
                 
